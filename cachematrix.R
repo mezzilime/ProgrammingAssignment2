d6fd01c348f02099f52dea32ca5fe37276d1a746
Mac Air <mezzilime@mm.st>
2014-07-27 23:54:06 +0800

    Revert "Added two functions to cachematrix.R"
    This reverts commit f5b634ffaa5408810b169b35a36a263c162cbe11.

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,15 @@
-## The functions calculate and cache an inverse of a matrix
+## Put comments here that give an overall description of what your
+## functions do
 
-## makeCacheMatrix caches the inverse of a matrix
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-            x <<- y
-            m <<- NULL
-        }
-        get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
-        list(set = set, get = get,
-            setinverse = setinverse,
-            getinverse = getinverse)
+
 }
 
-## cacheSolve returns an inverse of a matrix, 'x'
-## If the matrix has been calculated, the function returns the cached matrix
+
+## Write a short comment describing this function
+
 cacheSolve <- function(x, ...) {
-        m <- x$getinverse()
-        if(!is.null(m)) {
-            message("getting cached data")
-            return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        ## Return a matrix that is the inverse of 'x'
 }
